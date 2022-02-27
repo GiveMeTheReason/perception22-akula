6974437982a777ea30d7ea5aeb160dcf87d15611
GiveMeTheReason <botva2000@gmail.com>
2022-02-27 23:49:28 +0300

    arc kinematic model
    arc kinematic model

--- odometry.py
+++ odometry.py
@@ -4,11 +4,15 @@ import numpy as np
 def forward_kinematics(params: dict, state: np.ndarray, odometry: np.ndarray) -> np.ndarray:
     """
     Forward kinematics for differential platform
+    Arc Kinematic motion model (Prob.Rob.Ch5.3)
+    Assumption: motion consists of arcs
 
-    delta_state (3x1) = A (3x2) @ angle_wheel (2x1)
-    delta_state.T (1x3) = angle_wheel.T (1x2) @ A.T (2x3)
+    Arc length = k * (V1 + V2) / 2
+    Arc angle = k * (V1 - V2) / (2 * l)
+    k = 2 * pi * r / counts_2pi
+
     angle_wheel (scalar) = counts / counts_per_rotation * 2 * pi
-    angle_robot (scalar) = angle_wheel * r / R (if rotation on place)
+    angle_robot (scalar) = angle_wheel * r / R (R - rotation radius (L))
 
     :param params: Dict, robot geometry parameters
     :param state: Array-like object (1x3), robot current state [x, y, theta]
@@ -23,13 +27,19 @@ def forward_kinematics(params: dict, state: np.ndarray, odometry: np.ndarray) ->
     l = params['diag_length']
     r = params['wheel_radius']
     counts_2pi = params['counts_per_rotation']
-
     x, y, th = state
-    A = 1/2 * np.array([[np.cos(th), np.cos(th)],
-                        [np.sin(th), np.sin(th)],
-                        [1/l, -1/l]]).T
 
-    return 2 * r * np.pi / counts_2pi * odometry @ A
+    k = 2 * np.pi * r / counts_2pi
+    d_len_right = odometry[0] * k
+    d_len_left = odometry[1] * k
+    d_len = (d_len_right + d_len_left) / 2
+    d_angle = (d_len_right - d_len_left) / (2 * l)
+
+    d_state = np.array([d_len/d_angle * (-np.sin(th) + np.sin(th + d_angle)),
+                        d_len/d_angle * (np.cos(th) - np.cos(th + d_angle)),
+                        d_angle])
+
+    return state + d_state
 
 
 # metres
@@ -37,7 +47,7 @@ params = {'diag_length': 2,
           'wheel_radius': 0.5,
           'counts_per_rotation': 256}
 # initial state
-state = np.array([0., 0., np.pi/4])
+state = np.array([0., 0., 0.])
 
 # ticks (counts on every wheel)
 odometry = np.array([4*256., 2*256.])
